a416afaf7f2221083127d270114e745bff52366c
alexthayn <00148946@badgermail.snow.edu>
2018-02-05 20:55:59 -0700

    Phase 1

--- app/src/main/java/com/example/alexthayn1/testapp/CreateUserActivity.java
+++ app/src/main/java/com/example/alexthayn1/testapp/CreateUserActivity.java
@@ -2,6 +2,10 @@ package com.example.alexthayn1.testapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.view.View;
 
 public class CreateUserActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class CreateUserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_user);
+    }
+
+    public void onClick(View view){
+        EditText input = (EditText) findViewById(R.id.username);
+        String string = input.getText().toString();
+        Toast.makeText(this, "User " + string + " created.", Toast.LENGTH_LONG).show();
     }
 }
