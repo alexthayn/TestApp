01881d704667a78a46952f78b7b2ad038498a0b7
alexthayn <00148946@badgermail.snow.edu>
2018-02-06 00:16:45 -0700

    Final App

--- app/src/main/java/com/example/alexthayn1/testapp/CreateUserActivity.java
+++ app/src/main/java/com/example/alexthayn1/testapp/CreateUserActivity.java
@@ -4,10 +4,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Toast;
 import android.view.View;
 
 public class CreateUserActivity extends AppCompatActivity {
+    boolean male = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +18,31 @@ public class CreateUserActivity extends AppCompatActivity {
         setContentView(R.layout.activity_create_user);
     }
 
+
     public void onClick(View view){
         EditText input = (EditText) findViewById(R.id.username);
         String string = input.getText().toString();
-        Toast.makeText(this, "User " + string + " created.", Toast.LENGTH_LONG).show();
+
+
+
+        final RadioGroup radioGroup = (RadioGroup) findViewById(R.id.gender);
+        radioGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                switch (checkedId) {
+                    case R.id.male:
+                        male = true;
+                        RadioButton m = (RadioButton) findViewById(R.id.male);
+                        break;
+                    case R.id.female:
+                        male = false;
+                        RadioButton f = (RadioButton) findViewById(R.id.female);
+                        break;
+                }
+            }
+        });
+        RadioButton rb = (RadioButton) findViewById(radioGroup.getCheckedRadioButtonId());
+
+        Toast.makeText(this,rb.getText()+ " User " + string + " created.", Toast.LENGTH_LONG).show();
     }
 }
